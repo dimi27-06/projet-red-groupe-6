56572b652baa0feb204540bb0fac14132705726a
dimi <dimitri.manfredonia@ynov.com>
2025-09-14 23:58:05 +0200

    projet red

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	p1 := Character{}  // Création d’un personnage vide
-	p1.initCharacter() // Initialisation avec valeurs par défaut
-	p1.MainMenu()      // Lancement du menu principal
-}
